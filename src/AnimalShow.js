b6c7b948d871d3426f22a650c0de4bfbf7f7ced9
Indira Diana Puspasari <indiradiana239@gmail.com>
2023-05-29 16:43:50 +0700

    add hearts

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,5 +1,34 @@
+import { useState } from "react";
+import bird from "./Images/bird.svg";
+import cat from "./Images/cat.svg";
+import cow from "./Images/cow.svg";
+import dog from "./Images/dog.svg";
+import gator from "./Images/gator.svg";
+import heart from "./Images/heart.svg";
+import horse from "./Images/horse.svg";
+
+const svgMap = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  horse,
+};
+
 function AnimalShow({ type }) {
-  return <div>{type}</div>;
+  const [clicks, setClicks] = useState(0);
+
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+
+  return (
+    <div onClick={handleClick}>
+      <img alt="animals" src={svgMap[type]} />
+      <img alt="heart" src={heart} style={{ width: 10 + 10 * clicks + "px" }} />
+    </div>
+  );
 }
 
 export default AnimalShow;
